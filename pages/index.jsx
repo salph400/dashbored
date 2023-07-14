1b9f5ceb78b24df89f2cb968c87c6d9f69a10be6
salph400 <107527855+salph400@users.noreply.github.com>
2023-07-14 19:46:19 -0400

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,10 +10,10 @@ export default function Index() {
       <img width="500" src="/img/bck.png" />
       </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Treo
+          Bloxzy
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-      Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features. 
+      Bloxzy is a multipurpose bot for your discord server with Stocks Values, Calculator and more!
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
@@ -23,13 +23,13 @@ export default function Index() {
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Invite Treo
+          Invite Bloxzy
               </a>
             </Link>
   <div className="py-10"></div>
   <Link href={"https://top.gg/bot/1016392200516550736/vote"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote Treo</a>
+Vote Bloxzy</a>
   </Link>
         </div>
 
@@ -48,7 +48,7 @@ Vote Treo</a>
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Treo Bot" }} />
             <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
             <a href="https://discord.com/oauth2/authorize?client_id=1004699301982773259&permissions=8&scope=bot%20applications.commands" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Invite Treo
+              <i className="fab fa-discord mr-2" />Invite Bloxzy
             </a>
           </div>
 
@@ -63,7 +63,7 @@ Vote Treo</a>
               <div>
                 <div className="text-white">
                   <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Universal</p>
-                  <p className="text-gray-500 line-clamp-4">Treo is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features.</p>
+                  <p className="text-gray-500 line-clamp-4">Bloxzy is a multipurpose bot for your discord server with Moderation, Fun, Giveaway, Economy, Utility etc... features.</p>
                 </div>
               </div>
               <div>
