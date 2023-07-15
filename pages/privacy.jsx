d761fe36b4c08f3f78eb241baf9bd1a552a16383
salph400 <107527855+salph400@users.noreply.github.com>
2023-07-14 20:30:56 -0400

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -2,7 +2,7 @@ export default function Privacy() {
 
     return (
         <div>
-            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Treo</h1>
+            <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Bloxzy</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
         <h2 className="font-semibold text-3xl text-white/75">Usage Of Data</h2>
